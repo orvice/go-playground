f30af7ac760191893c90afac4f244f9726efdaa3
orvice <orvice@gmail.com>
2023-08-02 15:03:42 +0800

    json handler

--- cmd/slog/main.go
+++ cmd/slog/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"context"
 	"net"
+	"os"
 
 	"golang.org/x/exp/slog"
 )
 
 func main() {
-	// slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr)))
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)))
 	slog.Info("hello", "name", "Al")
 	slog.Error("oops", "status", 500)
 	ctx := context.Background()
